3336a66e2a45872766212997602100554d53efe5
nazimelmardi <London1983!>
2024-05-30 20:42:07 +0200

    basic css added

--- src/VehicleTable.js
+++ src/VehicleTable.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './index.css';
 
 const VehicleTable = ({ vehicles, onUpdateVehicle, onUpdateMessage, onRegisterVehicle }) => {
   return (
     <div>
-      <table>
+      <table className="vehicle-table">
         <thead>
         <tr>
           <th>ID</th>
